c0eec951d9c35730c896d2b84bfb7bdde1313bb8
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2026-08-16 22:01:13 +0200

    Ajout fonction de changement de status

--- popup.js
+++ popup.js
@@ -1,1 +1,19 @@
 const btn = document.getElementById("btn")
+btn.addEventListener("click", changeStatus)
+
+function changeStatus() {
+    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    if (!tab?.id) return;
+
+    chrome.tabs.sendMessage(
+        tab.id,
+        { action: "OnOff", color: "yellow" },
+        (response) => {
+            if (chrome.runtime.lastError) {
+                console.error("Error:", chrome.runtime.lastError.message);
+                return;
+            }
+            console.log("Response from content script:", response);
+        }
+    );
+}
